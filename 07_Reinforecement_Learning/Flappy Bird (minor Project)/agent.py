787e638db6dee6709c9f6943ec231310e8a682c2
CodeByMehra <kpvishalmehra@gmail.com>
2026-06-30 14:22:22 +0530

    yamll

--- 07_Reinforecement_Learning/Flappy Bird (minor Project)/agent.py
+++ 07_Reinforecement_Learning/Flappy Bird (minor Project)/agent.py
@@ -1,6 +1,8 @@
 import flappy_bird_gymnasium
 import gymnasium as gym
 from dqn import DQN
+from experence_replay import ReplayMemory
+import itertools
 
 if torch.backends.mps.is_available():
     device = "mps"
@@ -19,17 +21,32 @@ def run(self, is_training=True, render = False):
     policy_dqn = DQN(num_states, num_actions).to(device)
 
 
-    obstates, _ = env.reset()
-    while True:
-        # Next action:
-        # (feed the observation to your agent here)
-        action = env.action_space.sample()
 
-        # Processing:  We will use terminated as done variable
-        next_state, reward, terminated, _, _ = env.step(action)
+    if is_training:
+        memory = ReplayMemory(10000)
+
+    for episode in itertools.count():
+
+        state, _ = env.reset()
+        episode_rewards = 0
+
+        while not terminated:
+            # Next action:
+            # (feed the observation to your agent here)
+            action = env.action_space.sample()
+
+            # Processing:  We will use terminated as done variable
+            next_state, reward, terminated, _, _ = env.step(action)
+
+            if is_training:
+                memory.append((state, action, new_state, reward,  terminated))
+
+
+                state = new_state
+                episode_rewards += rewards
+
+        print(f" for episode = {episode +1} with total reward = {episode_rewards}")
+
         
-        # Checking if the player is still alive
-        if terminated:
-            break
 
-    env.close() 
+    # env.close() - we will stop manually
